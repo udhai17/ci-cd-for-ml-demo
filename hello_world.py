5100883a9aa1fc141512ec62fbb3403702117573
udhai <udhai170819@gmail.com>
2023-11-29 23:22:38 +0530

    added hello-world script

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+import datetime
+
+
+def main():
+    print("Hello, World !")
+
+    current_time = datetime.datetime.now()
+    print(f"current time is {current_time}")
+
+
+if __name__ == "__main__":
+    main()
